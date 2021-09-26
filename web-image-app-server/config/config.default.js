b9a07c9a8c760e0956ff1c4a82d829e1846902af
张文畅 <715517322@qq.com>
2021-09-26 13:42:41 +0800

    chore(后端工程):添加模块和配置
    Added:
    - 添加egg-sequelize模块和依赖
    - 添加数据库信息配置
    - 添加上传文件限制配置
    - 提交package-lock.json

--- web-image-app-server/config/config.default.js
+++ web-image-app-server/config/config.default.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+const path = require('path');
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -18,11 +19,64 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ '*' ], // 配置白名单
+  };
+  config.cors = {
+    origin: '*', // 允许所有跨域访问
+    credentials: true, // 允许 Cookie 跨域跨域
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  };
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '118.31.62.229',
+    port: 3306,
+    database: 'test',
+    username: 'root',
+    password: 'Sanlink_123',
+  };
+  // 上传格式和大小限制
+  config.multipart = {
+    // fileSize: '50mb',
+    fileSize: 1048576000,
+    // mode: 'stream',
+    mode: 'file',
+    fileExtensions: [
+      // images
+      '.jpg', '.jpeg', // image/jpeg
+      '.png', // image/png, image/x-png
+      '.gif', // image/gif
+      '.bmp', // image/bmp
+      '.wbmp', // image/vnd.wap.wbmp
+      '.webp',
+      '.tif',
+      '.psd',
+      // tar
+      '.zip',
+      '.gz', '.tgz', '.gzip',
+    ],
+  };
+  config.logger = {
+    dir: '/root/egg-log',
   };
 
+  // add your user config here
+  const userConfig = {
+    imagePath:'/root/data/upload',
+  };
+  config.static = {
+    // 静态化访问前缀,如：`http://127.0.0.1:7001/static/images/logo.png`
+    prefix: '/',
+    dir: path.join(appInfo.baseDir, 'app/public'), // `String` or `Array:[dir1, dir2, ...]` 静态化目录,可以设置多个静态化目录
+    dynamic: true, // 如果当前访问的静态资源没有缓存，则缓存静态文件，和`preload`配合使用；
+    preload: false,
+    maxAge: 31536000, // in prod env, 0 in other envs
+    buffer: true, // in prod env, false in other envs
+  };
   return {
     ...config,
     ...userConfig,
