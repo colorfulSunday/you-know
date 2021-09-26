b578df635b8690d80f96a216995ddec85e2d5290
张文畅 <715517322@qq.com>
2021-09-26 18:28:04 +0800

    chore(前端工程):添加开发模式代理配置

--- web-image-app-client/vite.config.js
+++ web-image-app-client/vite.config.js
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        // 当遇到 /api 路径时，将其转换成 target 的值
+        target: 'http://118.31.62.229:8080/',
+        // target: 'http://127.0.0.1:8080/',
+      },
+      '/img': {
+        // 当遇到 /api 路径时，将其转换成 target 的值
+        target: 'http://118.31.62.229:8080/',
+        // target: 'http://127.0.0.1:7001/',
+      }
+    }
+  },
 })
