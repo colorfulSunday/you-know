9b3b6016eb5e28e66d66fffe5dc4c9ffa5b639e2
egg23333 <806261011@qq.com>
2021-09-26 15:21:16 +0800

    <feat>: 文件移动

--- web-image-app-client/bin/init.js
+++ web-image-app-client/bin/init.js
@@ -5,6 +5,7 @@ const clear=require('clear');
 const fs=require('fs');
 const path=require('path');
 const move=require('./move.js');
+const conf=require('./conf.js');
 const chalk=require('chalk');
 const log=(ctx=>console.log(chalk.white(ctx)));
 
@@ -29,13 +30,16 @@ const init=(async ()=>{
 	log('Frontend building...');
 	await execa('npm run build');
 	
+	//
 	log('Creating config...');
 	const config=await conf();
-	fs.writeFileSync('../web-image-server/config/config.js',config);
+	fs.writeFileSync('../../web-image-server/config/config.js',config);
 	
+	//
 	log('Moving ./dist to backend...');
 	await move();
 	
+	//
 	log('Starting serve...');
 	await execa('cd ../web-image-server/');
 	await execa('npm i');
--- web-image-app-client/bin/move.js
+++ web-image-app-client/bin/move.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 
+const fs=require('fs-extra');
+
 const move=(async()=>{
+	const src='../dist'
+	const target='../../web-image-server'
+	fs.copySync(src,target,(err=>{
+		if(err)
+			console.log(err);
+		console.log(src+'success');
+	});
 });
 
 module.exports=move;
