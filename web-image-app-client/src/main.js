1b5af6e79193d493d0c7cd3643164d327c3407df
taptaq <61082544+taptaq@users.noreply.github.com>
2021-09-26 17:12:27 +0800

    引入压缩插件

--- web-image-app-client/src/main.js
+++ web-image-app-client/src/main.js
@@ -7,5 +7,9 @@ import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
+// 引入压缩插件
+import compress from './JS/compress.js'
+app.config.globalProperties.$compress = compress
+
 app.use(ElementPlus)
 app.mount('#app')
